3767cce628f5491793162063b2bf381524ce6b64
dmitri <dmitrivitr@gmail.com>
2021-03-02 13:11:44 +0300

    less2

---
+++ app/src/main/java/com/example/myweatherdev/ui/main/model/RepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.myweatherdev.ui.main.model
+
+class RepositoryImpl : Repository {
+    override fun getWeatherFromServer(): Weather {
+        return Weather()
+    }
+
+    override fun getWeatherFromLocalStorage(): Weather {
+        return Weather()
+    }
+}
