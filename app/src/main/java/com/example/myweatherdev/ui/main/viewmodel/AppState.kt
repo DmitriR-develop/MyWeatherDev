8b297f5947e5006dccda36e0ab88fe00aa2a2754
dmitri <dmitrivitr@gmail.com>
2021-03-05 22:06:31 +0300

    less3

--- app/src/main/java/com/example/myweatherdev/ui/main/viewmodel/AppState.kt
+++ app/src/main/java/com/example/myweatherdev/ui/main/viewmodel/AppState.kt
@@ -3,7 +3,7 @@ package com.example.myweatherdev.ui.main.viewmodel
 import com.example.myweatherdev.ui.main.model.Weather
 
 sealed class AppState {
-    data class Success(val weatherData: Weather) : AppState()
+    data class Success(val weatherData: List<Weather>) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
 }
