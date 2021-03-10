1db889e750b6844d8ff8565005fe3e24ce7b735c
dmitri <dmitrivitr@gmail.com>
2021-03-10 18:13:19 +0300

    less5

--- app/src/main/java/com/example/myweatherdev/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/myweatherdev/ui/main/view/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.myweatherdev.ui.main.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myweatherdev.R
+import com.example.myweatherdev.databinding.MainActivityBinding
 import com.example.myweatherdev.ui.main.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+                .commitAllowingStateLoss()
         }
     }
 }
--- app/src/main/java/com/example/myweatherdev/ui/main/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/myweatherdev/ui/main/view/details/DetailsFragment.kt
@@ -1,20 +1,66 @@
 package com.example.myweatherdev.ui.main.view.details
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.myweatherdev.R
 import com.example.myweatherdev.databinding.FragmentDetailsBinding
 import com.example.myweatherdev.ui.main.model.Weather
-import com.example.myweatherdev.ui.main.viewmodel.AppState
-import com.example.myweatherdev.ui.main.viewmodel.MainViewModel
-import com.google.android.material.snackbar.Snackbar
+import com.example.myweatherdev.ui.main.model.WeatherDTO
 
 class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
+    private val onLoadListener: WeatherLoader.WeatherLoadListener =
+        object : WeatherLoader.WeatherLoadListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+            }
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+    }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityName.text = city.city
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                city.lat.toString(),
+                city.lon.toString()
+            )
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
+        }
+    }
 
     companion object {
 
@@ -27,30 +73,4 @@ class DetailsFragment : Fragment() {
         }
     }
 
-    private var _binding: FragmentDetailsBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            weather.city.also { city ->
-                binding.cityName.text = city.city
-                binding.cityCoordinates.text = String.format(
-                    getString(R.string.city_coordinates),
-                    city.lat.toString(),
-                    city.lon.toString()
-                )
-                binding.temperatureValue.text = weather.temperature.toString()
-                binding.feelsLikeValue.text = weather.feelsLike.toString()
-            }
-        }
-    }
 }
